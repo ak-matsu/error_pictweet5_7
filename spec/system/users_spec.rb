c6b02485ed0934bb3cca917094c8f45b9e3ae26b
ak-matsu <sandbox.matsu@gmail.com>
2021-02-16 22:26:41 +0900

    問題2の回答を記述

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'ログイン', type: :system do
-
+  #ログイン機能を確認する場合はbuildではなくcreateである
   before do
     @user = FactoryBot.create(:user)
   end
@@ -47,11 +47,24 @@ RSpec.describe 'ログイン', type: :system do
   context 'ログインができないとき' do
     it '保存されているユーザーの情報と合致しないとログインができない' do
       # トップページに移動する
+      visit root_path
+
       # トップページにログインページ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+
       # ログインページへ遷移する
+      visit new_user_session_path
+
       # ユーザー情報を入力する
+      fill_in "Email",with: ''
+      fill_in "Password",with:''
+
       # ログインボタンを押す
+      find('[name="commit"]').click
+
       # ログインページへ戻されることを確認する
+      
+      expect(current_path).to eq(new_user_session_path)
     end
   end
  end
