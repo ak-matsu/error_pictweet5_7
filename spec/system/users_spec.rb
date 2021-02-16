33407d96865e2d4561c697f7f7000184fa1d4e57
ak-matsu <sandbox.matsu@gmail.com>
2021-02-16 20:53:09 +0900

    問題の雛形を作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe 'ログイン', type: :system do
+  
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
-end
+  context 'ログインができるとき' do
+    it '保存されているユーザーの情報と合致すればログインができる' do
+      # トップページに移動する
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      # ログインページへ遷移する
+      # 正しいユーザー情報を入力する
+      # ログインボタンを押す
+      # トップページへ遷移することを確認する
+      # カーソルを合わせるとログアウトボタンが表示されることを確認する
+      # サインアップページへ遷移するボタンやログインページで遷移するボタンが表示されていないことを確認する
+    end
+  end
+ 
+  context 'ログインができないとき' do
+    it '保存されているユーザーの情報と合致しないとログインができない' do
+      # トップページに移動する
+      # トップページにログインページ遷移するボタンがあることを確認する
+      # ログインページへ遷移する
+      # ユーザー情報を入力する
+      # ログインボタンを押す
+      # ログインページへ戻されることを確認する
+    end
+  end
+ end
